f4aed9be95afb36c83cd3b856f42c3c234944809
Ivo Seitenfus Neto <iesneto@gmail.com>
2023-06-08 10:03:46 -0300

    Created script to handle collisons on Enemy

---
+++ Assets/Scripts/Gamob/SpaceShooter/Enemy/EnemyCollisions.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamob.SpaceShooter.Player;
+
+namespace Gamob.SpaceShooter.Enemy
+{
+    public class EnemyCollisions : MonoBehaviour
+    {
+        [SerializeField] private string _laserTag;
+        [SerializeField] private string _playerTag;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(_playerTag))
+            {
+                PlayerLives lives = other.GetComponent<PlayerLives>();
+
+                if(lives == null)
+                {
+                    return;
+                }
+
+                lives.Damage();
+                Destroy(gameObject);
+                return;
+            }
+            if (other.CompareTag(_laserTag))
+            {
+                Destroy(other.gameObject);
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+    }
+}
