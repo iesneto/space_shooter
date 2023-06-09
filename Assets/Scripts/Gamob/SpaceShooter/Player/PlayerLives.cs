134d25d130e2d25da1b19b1fb99564eb3d13f83d
Ivo Seitenfus Neto <iesneto@gmail.com>
2023-06-08 22:36:23 -0300

    Cominucate PlayerLives with spawnManager

--- Assets/Scripts/Gamob/SpaceShooter/Player/PlayerLives.cs
+++ Assets/Scripts/Gamob/SpaceShooter/Player/PlayerLives.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Gamob.SpaceShooter.Spawn;
 
 namespace Gamob.SpaceShooter.Player
 {
     public class PlayerLives : MonoBehaviour
     {
         [SerializeField] private int _lives;
+        private SpawnManager _spawnManager;
 
         public void Damage()
         {
             _lives--;
 
             CheckLives();
+        }
+
+        private void Start()
+        {
+            _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
         }
 
         private void CheckLives()
@@ -22,6 +29,11 @@ namespace Gamob.SpaceShooter.Player
                 return;
             }
 
+            if(_spawnManager != null)
+            {
+                _spawnManager.StopSpawning();
+            }
+            
             Destroy(gameObject);
         }
     }
