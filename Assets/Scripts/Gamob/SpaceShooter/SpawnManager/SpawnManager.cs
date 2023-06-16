6eb50f7d0856bba41d4beceb903370b01af3d5ab
Ivo Seitenfus Neto <iesneto@gmail.com>
2023-06-15 22:01:38 -0300

    Spawn TripleShots powerups

--- Assets/Scripts/Gamob/SpaceShooter/SpawnManager/SpawnManager.cs
+++ Assets/Scripts/Gamob/SpaceShooter/SpawnManager/SpawnManager.cs
@@ -7,6 +7,7 @@ namespace Gamob.SpaceShooter.Spawn
     public class SpawnManager : MonoBehaviour
     {
         [SerializeField] private GameObject _enemyPrefab;
+        [SerializeField] private GameObject _tripleShotPrefab;
         [SerializeField] private float _enemyFrequency;
         [SerializeField] private Transform _enemyContainer;
         private bool _stopSpawning = false;
@@ -19,10 +20,11 @@ namespace Gamob.SpaceShooter.Spawn
 
         private void Start()
         {
-            StartCoroutine(SpawnRoutine());
+            StartCoroutine(SpawnEnemy());
+            StartCoroutine(SpawnTripleShot());
         }
 
-        private IEnumerator SpawnRoutine()
+        private IEnumerator SpawnEnemy()
         {
             while (!_stopSpawning)
             {
@@ -32,6 +34,17 @@ namespace Gamob.SpaceShooter.Spawn
                 yield return new WaitForSeconds(_enemyFrequency);
             }
         }
+
+        private IEnumerator SpawnTripleShot()
+        {
+            while (!_stopSpawning)
+            {
+                Vector3 positionToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+                Instantiate(_tripleShotPrefab, positionToSpawn, Quaternion.identity);
+                yield return new WaitForSeconds(Random.Range(7, 11));
+            }
+        }
+            
     }
 }
 
