a7440e150a07e4f3625bfc152f99d9ec2f21acbf
Ivo Seitenfus Neto <iesneto@gmail.com>
2023-06-15 22:01:58 -0300

    powerup behavior

--- Assets/Scripts/Gamob/SpaceShooter/PowerUp/PowerUp.cs
+++ Assets/Scripts/Gamob/SpaceShooter/PowerUp/PowerUp.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Gamob.SpaceShooter.Player;
 
 namespace Gamob.SpaceShooter.Powerup
 {
     public class PowerUp : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] private float _speed;        
 
-        }
-
-        // Update is called once per frame
         void Update()
         {
+            Move();
+        }
 
+        private void Move()
+        {
+            transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+            if(transform.position.y < -4.5f)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                PlayerShooting playerShooting = other.GetComponent<PlayerShooting>();
+                if (playerShooting != null)
+                {
+                    playerShooting.ActivateTripleShot();
+                }
+                Destroy(gameObject);
+            }
         }
     }
 }
